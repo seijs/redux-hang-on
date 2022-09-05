292807dd21f65c403dd00b4b5759b80bfa05f766
Konstantin Astapov <asapovk@gmail.com>
2022-09-05 13:08:44 +0300

    upd example

--- examples/sandbox/src/app/reducers/loadEventsDone.reducer.ts
+++ examples/sandbox/src/app/reducers/loadEventsDone.reducer.ts
@@ -1,5 +1,7 @@
+import { MakeBiteReducerType } from "@seijs/redux-hang-on/lib/types";
+import { IAppState, IAppTriggers } from "../app.config";
 
-export const loadAppDone = (state, payload) => {
+export const loadAppDone: MakeBiteReducerType<IAppTriggers, IAppState, 'loadApp'>['done'] = (state, payload) => {
   if (typeof state.app === 'number') {
     state.app += payload as number;
   } else {
--- examples/sandbox/src/app/reducers/loadEventsWait.reducer.ts
+++ examples/sandbox/src/app/reducers/loadEventsWait.reducer.ts
@@ -1,4 +1,6 @@
+import { MakeBiteReducerType } from "@seijs/redux-hang-on/lib/types";
+import { IAppState, IAppTriggers } from "../app.config";
 
-export const loadAppWait = (state, payload) => {
+export const loadAppWait: MakeBiteReducerType<IAppTriggers, IAppState, 'loadApp'>['wait'] = (state, payload) => {
   state.loading = true;
 };
