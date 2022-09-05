6783d1ca65549d070d3dce617a8d6f80b2193a51
Konstantin Astapov <asapovk@gmail.com>
2022-09-05 10:01:27 +0300

    fix useTrigger

--- examples/sandbox/src/_redux/useTrigger.ts
+++ examples/sandbox/src/_redux/useTrigger.ts
@@ -1,9 +1,15 @@
 import { useDispatch } from 'react-redux';
 import { ITriggers } from './types';
 import { DispatcherType } from '@seijs/redux-hang-on/lib/types';
+import { getActionType } from '@seijs/redux-hang-on/utils'
 
 export const useTrigger = () => {
-  const trigger = useDispatch() as DispatcherType<ITriggers>;
+  const dispatch = useDispatch();
 
+  const trigger: DispatcherType<ITriggers> = (trigger, status, payload) => {
+    const combynedType = getActionType(trigger, status);
+    dispatch({ type: combynedType, payload });
+  };
+  
   return trigger;
 };
