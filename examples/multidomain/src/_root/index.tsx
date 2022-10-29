36f3d7a2891c1878078dbf98ea498266d1b4c6a3
Konstantin Astapov <asapovk@gmail.com>
2022-10-29 16:49:15 +0300

    start

--- examples/multidomain/src/_root/index.tsx
+++ examples/multidomain/src/_root/index.tsx
@@ -5,10 +5,14 @@ import { Provider } from 'react-redux';
 import store from '../_redux';
 import { ErrorBoundary } from '../_components';
 
-
-
 import { LettersList } from 'src/letters/components/LettersList';
 import { Notification } from 'src/notification/components/Notification';
+
+/*
+** In MULD-4 we implement popup window 
+** and mechanism that open popup when close compose without saving
+**
+*/
 
 const Application = () => (
   <ErrorBoundary>
@@ -20,3 +24,4 @@ const Application = () => (
 );
 
 ReactDOM.render(<Application />, document.getElementById('app'));
+
